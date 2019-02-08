98db9d9b621bb8ebdd4680b77d6be64e39ecc167
languantan <languan.tan@gmail.com>
2019-02-08 17:34:54 +0800

    Create totp.py
    add totp.py

---
+++ totp.py
@@ -0,0 +1,56 @@
+"""
+File: totp.py
+Author: yanfly
+Email: languantan@gmail.com
+Github: https://github.com/languantan
+Description: main file to talk to tunnelblick
+"""
+
+from subprocess import Popen, PIPE
+import pyotp
+import keyring
+
+VPN_SERVERS = ['profile1', 'profile2', 'profile3']
+
+
+def main():
+    """TODO: Docstring for main.
+    :returns: TODO
+
+    """
+    print(' '.join([f'{i}) {x}' for i, x in enumerate(VPN_SERVERS)]))
+
+    vpn_selection = int(input('Select VPN server: '))
+
+    vpn_server = VPN_SERVERS[vpn_selection]
+
+    vpn_key = keyring.get_password('totp-cli', vpn_server)
+    if not vpn_key:
+        print('No keyring found')
+        vpn_key = input('Enter VPN OTP key:')
+        keyring.set_password('totp-cli', vpn_server, vpn_key)
+
+    totp = pyotp.TOTP(vpn_key)
+    clipboard = Popen('pbcopy', stdin=PIPE)
+    clipboard.communicate(totp.now().encode())
+    print(totp.now() + ' copied to clipboard')
+
+    connect_script = f'''
+    tell application "Tunnelblick"
+        connect "{vpn_server}"
+        get state of first configuration where name = "{vpn_server}"
+        repeat until result = "CONNECTED" or result = "EXITING"
+            delay 1
+            get state of first configuration where name = "{vpn_server}"
+        end repeat
+    end tell'''
+
+    applescript = Popen(['osascript'], stdin=PIPE)
+    applescript.communicate(connect_script.encode())
+
+    print('')
+    print('give thanks to yanfly,broly')
+
+
+if __name__ == "__main__":
+    main()
